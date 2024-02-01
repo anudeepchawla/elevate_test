7ec16a31d26b416604cba4dc20ddd0100f45b4d6
Anudeep Chawla <anudeep.chawla@berkeley.edu>
2024-02-01 14:40:18 -0800

    Initial Commit

---
+++ elevate_test/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  elevate_test
+//
+//  Created by Anudeep Chawla on 2/1/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
